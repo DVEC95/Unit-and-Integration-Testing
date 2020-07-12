f61ef8c054cb94a5f62ab0ee8c6266db4cf65720
David Eastcroft <davideastcroft95@gmail.com>
2020-07-12 22:24:04 +0100

    Adds test for updating display.

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -10,10 +10,21 @@ describe('calculator functionality', function() {
   });
 
   // write integration tests here in the form of "it should do something..."
-  it('should have working number buttons', function(){
+  it('it should have working number buttons', function(){
     running_total = element(by.css('#running_total'))
     element(by.css('#number2')).click();
     expect(running_total.getAttribute('value')).to.eventually.equal('2')
-  })
+  });
+
+  it("it should update the display with the result of arithmetical operations", function(){
+    running_total = element(by.css("#running_total"));
+    element(by.css("#number1")).click();
+    element(by.css("#operator_add")).click();
+    element(by.css("#number2")).click();
+    element(by.css("#operator_equals")).click();
+    expect(running_total.getAttribute("value")).to.eventually.equal("3");
+  });
+
+  
 
 });
